013bafb89ab015302e1237e1015be4fb5fb0ab7e
nobSEIFO <bezai2002@gmail.com>
2023-03-03 17:22:34 +0100

    first

---
+++ TD ex2.py
@@ -0,0 +1,7 @@
+import os
+os.system("cls" if os.name == "nt"else "clear")
+
+name=input("your name is :")
+LASTname=input("your LASTname is :")
+age=input("your age is :")
+print("YOUR DATA HAS BEEN STOLEN")
